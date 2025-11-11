0651843a1ba83175d08c3238f63f63c8dc50ec75
TerrificTing <jting200@yahoo.com>
2025-11-11 15:52:30 -0500

    Updated experience

--- data/experience.go
+++ data/experience.go
@@ -18,6 +18,7 @@ var ExperienceData = []Experience{
 		EndDate:  	"Present",
 		KeyPointList: []string{
 			"Addressed customer issues and optimzed workflows to improve efficiency",
+			"Managed customer serivce, food prepr, and inventory while maintaining organization",
 			"Collaborated effectively in a fast-paced environment",
 		},
 	},
